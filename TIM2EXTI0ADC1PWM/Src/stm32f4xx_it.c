49ee55b7af5242c9f5825de5343fe1ef13d1768a
lorenzoborba <lorenzoborba@alunos.utfpr.edu.br>
2025-05-12 22:08:50 -0300

    final commit

--- TIM2EXTI0ADC1PWM/Src/stm32f4xx_it.c
+++ TIM2EXTI0ADC1PWM/Src/stm32f4xx_it.c
@@ -219,16 +219,13 @@ void EXTI0_IRQHandler(void)
 void ADC_IRQHandler(void)
 {
   /* USER CODE BEGIN ADC_IRQn 0 */
-
  uint16_t adc1in1;
   /* USER CODE END ADC_IRQn 0 */
   HAL_ADC_IRQHandler(&hadc1);
   /* USER CODE BEGIN ADC_IRQn 1 */
-
   adc1in1 = HAL_ADC_GetValue(&hadc1);
   TIM2->CCR1 = adc1in1;
   /* USER CODE END ADC_IRQn 1 */
-}
  }
 
 /**
@@ -241,7 +238,6 @@ void TIM2_IRQHandler(void)
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
-
   HAL_ADC_Start_IT(&hadc1);
   /* USER CODE END TIM2_IRQn 1 */
 }
